86a0ac70de836577bca1026a9d88c5de7d18e73d
Rodrigo <roprgm@gmail.com>
2026-06-24 17:30:09 +0000

    Fix mobile input zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,8 @@ export const viewport: Viewport = {
   themeColor: "#000000",
   width: "device-width",
   initialScale: 1,
-  maximumScale: 5,
+  maximumScale: 1,
+  userScalable: false,
   viewportFit: "cover",
 };
 
